e3b765d05bfdcdf424c0f1647dbde506a350cfe1
ikedahideharu <hideharu.ikeda@gmail.com>
2023-10-07 13:58:20 +0900

    01

--- .github/actions/spreadsheets-push-action/index.ts
+++ .github/actions/spreadsheets-push-action/index.ts
@@ -5,17 +5,9 @@ try {
   const gt = getOctokit(process.env?.GITHUB_TOKEN || '')
   console.log(context.payload)
   console.log(process.env)
-
-  // PRのリンク作成
-  // 作業者を取得
-  // タイトル取得
-  // 日付を取得
-  // JIRAのURL
-
-  // スプレッドシートに書き込み
 } catch (error) {
-  console.error(error)
   if (error instanceof Error) {
+    console.error(error)
     setFailed(error?.message || '')
   }
 }
